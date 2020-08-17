db8e625fe7e2bfca0516ffaef660fa7fb3c391cf
arteaprogramar <arte.aprogramar@gmail.com>
2020-08-17 14:28:46 -0500

    AdNative v1.0.1 (Button bug fixed)

--- app/src/main/java/arte/programar/adnative/test/MainActivity.java
+++ app/src/main/java/arte/programar/adnative/test/MainActivity.java
@@ -3,7 +3,6 @@ package arte.programar.adnative.test;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
@@ -23,20 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         MobileAds.initialize(getApplicationContext());
 
-        final AdNativeView template = findViewById(R.id.adnative_medium);
-        template.createAdMediumCustom(
-                "Lorem ipsum dolor",
-                "Ut enim ad minim veniam, quis nostrud exercitation",
-                "Duis aute irure",
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_star),
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.rectangle)
-        );
+        final AdNativeView template = findViewById(R.id.adnative_small);
+        /**template.createAdMediumCustom(
+         "Lorem ipsum dolor",
+         "Ut enim ad minim veniam, quis nostrud exercitation",
+         "Duis aute irure",
+         ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_star),
+         ContextCompat.getDrawable(getApplicationContext(), R.drawable.rectangle)
+         );**/
 
         AdLoader loader1 = new AdLoader.Builder(this, ID_NATIVE)
                 .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
                     @Override
                     public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
-                        //AdNativeView template = findViewById(R.id.adnative_small);
                         template.setNativeAd(unifiedNativeAd);
                     }
                 })
